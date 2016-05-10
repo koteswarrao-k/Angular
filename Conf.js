109c801ae7de8dedfa04c6c503f614a415b01bbc
Kolluru <koteswarrao.kolluru@capgemini.com>
2016-05-10 17:49:40 +0530

    conf changes

---
+++ Conf.js
@@ -0,0 +1,11 @@
+exports.config = {
+  framework: 'jasmine2',
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['testFiles/spec.js'],
+capabilities: {
+  'browserName': 'firefox'
+}
+};
+
+
+
